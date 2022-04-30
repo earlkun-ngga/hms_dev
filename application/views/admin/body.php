53e1f047722061ce66ce55e09875910f7bb41428
unknown <tkj2.0erlangga.sa@gmail.com>
2022-04-30 20:24:21 +0700

    Hapus Asset

--- application/views/admin/body.php
+++ application/views/admin/body.php
@@ -5,7 +5,7 @@
 						<!--begin::Content Wrapper-->
 						<div class="main d-flex flex-column flex-row-fluid">
 							<div class="content flex-column-fluid" id="kt_content">
-								
+								<h1>Hello, Its update from github</h1>
 							</div>
 							<!--end::Content-->
 						</div>
